3a496ac3aff4fdecded5afa5176cf84bcc8d575a
ha-y0ung-kim <hayoungkim610@gmail.com>
2021-11-30 22:53:10 +0100

    reading image

--- main_imgp.cpp
+++ main_imgp.cpp
@@ -3,15 +3,14 @@
 #include <stdlib.h>
 
 #define STB_IMAGE_IMPLEMENTATION
-#include "stb_image/stb_image.h"
+#include "stb_image.h"
 
-main()
+int main()
 {
     int width, height, channels;
-    uint8_t *rgb_image = stbi_load("image.png", &width, &height, &CHANNELS, 3);
+    uint8_t *rgb_image = stbi_load("img_lenna.png", &width, &height, &channels, 3);
 
-    //unsigned char *img = stbi_load("sky.jpg", &width, &height, &channels, 0);
-    if (img == NULL)
+    if (rgb_image == NULL)
     {
         printf("Error in loading the image\n");
         exit(1);
@@ -20,5 +19,4 @@ main()
     printf("Loaded image with a width of %dpx, a height of %dpx and %d channels\n", width, height, channels);
 
     stbi_image_free(rgb_image);
-    //https://solarianprogrammer.com/2019/06/10/c-programming-reading-writing-images-stb_image-libraries/
 }
