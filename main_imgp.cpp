88a675949bc694cee2438e90141cd8dcc0e6f522
ha-y0ung-kim <hayoungkim610@gmail.com>
2021-11-30 23:34:23 +0100

    writing image

--- main_imgp.cpp
+++ main_imgp.cpp
@@ -3,7 +3,10 @@
 #include <stdlib.h>
 
 #define STB_IMAGE_IMPLEMENTATION
-#include "stb_image.h"
+#include "stb/stb_image.h"
+
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb/stb_image_write.h"
 
 int main()
 {
@@ -18,5 +21,8 @@ int main()
 
     printf("Loaded image with a width of %dpx, a height of %dpx and %d channels\n", width, height, channels);
 
+    stbi_write_png("test.png", width, height, channels, rgb_image, width * channels);
+
+    //int stbi_write_png(char const *filename, int w, int h, int comp, const void *data, int stride_in_bytes);
     stbi_image_free(rgb_image);
 }
