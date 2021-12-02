d47db56de934e8cd3a17afd187d5a267acf0f1dd
ha-y0ung-kim <hayoungkim610@gmail.com>
2021-12-02 10:59:39 +0100

    add mean_filter

--- main.cpp
+++ main.cpp
@@ -2,11 +2,27 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <algorithm>
 
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb/stb_image.h"
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb/stb_image_write.h"
+
+std::vector<std::vector<std::vector<uint8_t>>> convolution(std::vector<std::vector<std::vector<uint8_t>>> image, std::vector<std::vector<std::vector<uint8_t>>> kernel)
+{
+}
+
+std::vector<std::vector<std::vector<uint8_t>>> mean_filter(int kernal_size, int num_channel)
+{
+    double kernal_value = 1 / (kernal_size * kernal_size);
+    std::vector<std::vector<std::vector<uint8_t>>> output(kernal_size, std::vector<std::vector<uint8_t>>(kernal_size, std::vector<uint8_t>(num_channel)));
+    std::fill(output.begin(), output.end(), kernal_value);
+    return output;
+}
+
+
+
 
 int main()
 {
