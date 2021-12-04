3c1fe5de22f5383c90f60a918dddc5cb7b634627
ha-y0ung-kim <hayoungkim610@gmail.com>
2021-12-04 23:39:30 +0100

    vector types for simple writing

---
+++ vectortypes.h
@@ -0,0 +1,5 @@
+#include <vector>
+
+typedef std::vector<std::vector<uint8_t>> vector2d;
+typedef std::vector<std::vector<double>> vector2dd; // for vector of double
+typedef std::vector<std::vector<std::vector<uint8_t>>> vector3d;
