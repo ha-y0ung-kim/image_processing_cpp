ea1d3c6c72320784bc8634a659c165a051e2b8bf
ha-y0ung-kim <hayoungkim610@gmail.com>
2021-12-05 11:24:42 +0100

    add user filter selection

--- main.cpp
+++ main.cpp
@@ -11,18 +11,68 @@
 #include "header_files/printvector.h"
 #include "header_files/vectoroperation.h"
 #include "header_files/vectortypes.h"
+#include "header_files/kernel_input.h"
 
 int main()
 {
 
     vector3d grey_image = open_image_as_greyscale();
+    std::cout << "select filter type : " << std::endl;
+    std::cout << "Type (1) for Mean blurring" << std::endl;
+    std::cout << "Type (2) for Gaussian blurring" << std::endl;
+    std::cout << "Type (3) for Laplacian edge detection" << std::endl;
+    std::cout << "Type (4) for unsharp image" << std::endl;
 
-    // int kernel_size = 11;
-    // std::vector<std::vector<double>> mean_kern;
-    // mean_kern = mean_filter(kernel_size);
-    // // print_2dvector(mean_kern);
-    // std::vector<std::vector<std::vector<uint8_t>>> averaged_image;
-    // averaged_image = convolution(grey_image, mean_kern, false);
+    int num;
+    std::cin >> num;
+
+    if (num == 1)
+    {
+        int kernel_size = type_kernel_size();
+        vector2dd mean_kern;
+        mean_kern = mean_filter(kernel_size);
+        vector3d averaged_image;
+        averaged_image = convolution(grey_image, mean_kern, false);
+        export_image(averaged_image);
+    }
+    else if (num == 2)
+    {
+        int kernel_size = type_kernel_size();
+
+        std::cout << "Type gaussian variance : " << std::endl;
+        double kernel_sigma;
+        std::cin >> kernel_sigma;
+        vector2dd gaussian_kern;
+        gaussian_kern = gaussian_filter(kernel_size, kernel_sigma);
+        vector3d gaussian_image;
+        gaussian_image = convolution(grey_image, gaussian_kern, false);
+        export_image(gaussian_image);
+    }
+
+    else if (num == 3)
+    {
+        int kernel_size = type_kernel_size();
+
+        vector2dd laplacian_kern;
+        laplacian_kern = laplacian_filter(kernel_size);
+        vector3d laplacian_image;
+        laplacian_image = convolution(grey_image, laplacian_kern, true);
+        export_image(laplacian_image);
+    }
+
+    else if (num == 4)
+    {
+        int kernel_size = type_kernel_size();
+
+        vector2dd laplacian_kern;
+        laplacian_kern = laplacian_filter(kernel_size);
+        vector3d laplacian_image, unsharp_image;
+        laplacian_image = convolution(grey_image, laplacian_kern, true);
+        export_image(laplacian_image);
+
+        unsharp_image = grey_image + 0.3 * laplacian_image;
+        export_image(unsharp_image);
+    }
 
     // std::vector<std::vector<std::vector<uint8_t>>> edge_image_x, edge_image_y, edge_image;
     // std::vector<std::vector<double>> sobal_kernel;
@@ -30,26 +80,4 @@ int main()
     // print_2dvector(sobal_kernel);
     // edge_image_x = convolution(grey_image, sobal_kernel, true);
     // edge_image_y = convolution(grey_image, sobal_filter_y(), true);
-
-    // int kernel_size = 21;
-    // double kernel_sigma = 5;
-    // std::vector<std::vector<double>> gaussian_kern;
-    // gaussian_kern = gaussian_filter(kernel_size, kernel_sigma);
-    // print_2dvector(gaussian_kern);
-
-    // std::vector<std::vector<std::vector<uint8_t>>> gaussian_image;
-    // gaussian_image = convolution(grey_image, gaussian_kern, false);
-
-    // int kernel_size = 5;
-    // std::vector<std::vector<double>> laplacian_kern;
-    // laplacian_kern = laplacian_filter(kernel_size);
-    // // print_2dvector(laplacian_kern);
-
-    // std::vector<std::vector<std::vector<uint8_t>>> laplacian_image;
-    // laplacian_image = convolution(grey_image, laplacian_kern, true);
-    // std::vector<std::vector<std::vector<uint8_t>>> unsharp_image(width, std::vector<std::vector<uint8_t>>(height, std::vector<uint8_t>(1)));
-
-    // unsharp_image = grey_image + 0.3 * laplacian_image;
-
-    export_image(grey_image);
 }
