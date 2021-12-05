abe443272f05088e2fac847ddcea9a8361c3ff98
ha-y0ung-kim <hayoungkim610@gmail.com>
2021-12-05 10:12:51 +0100

    add function description

--- header_files/filters.h
+++ header_files/filters.h
@@ -5,6 +5,10 @@
 
 vector2dd mean_filter(int kernel_size)
 {
+    /*
+    create a mean filter of size (kernel_size * kernel_size)
+    blurrs the image
+    */
     double kernel_value = 1 / ((double)kernel_size * (double)kernel_size);
     vector2dd mean_kernel(kernel_size, std::vector<double>(kernel_size, kernel_value));
     return mean_kernel;
@@ -24,6 +28,10 @@ vector2dd sobal_filter_y()
 
 vector2dd laplacian_filter(int kernel_size)
 {
+    /*
+    create a laplacian_filter of size (kernel_size * kernel_size)
+    edge detection of an image
+    */
     vector2dd laplacian_kernel(kernel_size, std::vector<double>(kernel_size, -1.0));
     laplacian_kernel[kernel_size / 2][kernel_size / 2] = kernel_size * kernel_size - 1;
     return laplacian_kernel;
@@ -31,6 +39,10 @@ vector2dd laplacian_filter(int kernel_size)
 
 vector2dd gaussian_filter(int kernel_size, double sigma)
 {
+    /*
+    create a gaussian filter of size (kernel_size * kernel_size) and variance sigma
+    blurrs the image
+    */
     vector2dd gaussian_kernel(kernel_size, std::vector<double>(kernel_size, 0));
     double sum = 0.0;
     for (int i = 0; i < kernel_size; i++)
@@ -45,7 +57,7 @@ vector2dd gaussian_filter(int kernel_size, double sigma)
     {
         for (int j = 0; j < kernel_size; j++)
         {
-            gaussian_kernel[i][j] /= sum;
+            gaussian_kernel[i][j] /= sum; // Normalize the filter
         }
     }
     return gaussian_kernel;
--- header_files/imageload.h
+++ header_files/imageload.h
@@ -11,36 +11,43 @@
 
 vector3d load_image()
 {
-
+    /*
+    load an image into a 3d vector
+    ask for an input image file name with type .PNG
+    output
+        image: 3d vector of the image
+            the First dimension contains the R, G, B values of each pixel
+            the second, and third dimension contains the pixel value of each row and the height
+    */
     std::string filename;
     std::cout << "type input file name : ";
     std::cin >> filename;
     const char *img_filename = filename.c_str();
-    int width, height, channels;
+    int width, height, num_channels;
 
-    uint8_t *img = stbi_load(img_filename, &width, &height, &channels, 0);
+    uint8_t *img = stbi_load(img_filename, &width, &height, &num_channels, 0);
     while (img == NULL)
     {
         std::cout << "Error in loading the image" << std::endl;
         std::cout << "type input file name : " << std::endl;
         std::cin >> filename;
         const char *img_filename = filename.c_str();
-        img = stbi_load(img_filename, &width, &height, &channels, 0);
+        img = stbi_load(img_filename, &width, &height, &num_channels, 0);
     }
 
     std::cout << "Loaded image"
               << "width : " << width << ",  height : " << height
-              << ", number of channels : " << channels << std::endl;
+              << ", number of channels : " << num_channels << std::endl;
 
-    vector3d image(channels, vector2d(height, std::vector<uint8_t>(width)));
+    vector3d image(num_channels, vector2d(height, std::vector<uint8_t>(width)));
 
-    for (int k = 0; k < channels; k++)
+    for (int k = 0; k < num_channels; k++)
     {
         for (int i = 0; i < height; i++)
         {
             for (int j = 0; j < width; j++)
             {
-                image[k][i][j] = *(img + (j + width * i) * channels + k);
+                image[k][i][j] = *(img + (j + width * i) * num_channels + k);
             }
         }
     }
@@ -50,18 +57,23 @@ vector3d load_image()
 
 void export_image(const vector3d &image)
 {
-    int channels = image.size();
+    /*
+    export an image into a .PNG file
+    input
+        image : image to be exported
+    */
+    int num_channels = image.size();
     int height = image[0].size();
     int width = image[0][0].size();
 
-    uint8_t out[width * height * channels] = {0};
+    uint8_t out[width * height * num_channels] = {0};
 
     int a = 0;
     for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)
         {
-            for (int k = 0; k < channels; k++)
+            for (int k = 0; k < num_channels; k++)
             {
                 out[a] = image[k][i][j];
                 a += 1;
@@ -72,11 +84,14 @@ void export_image(const vector3d &image)
     uint8_t *final_img;
     final_img = out;
 
-    stbi_write_png("test.png", width, height, channels, final_img, width * channels);
+    stbi_write_png("test.png", width, height, num_channels, final_img, width * num_channels);
 }
 
 vector3d img_to_greyscale(const vector3d &image)
 {
+    /*
+    Change an image into greyscale
+    */
     int num_channels = image.size();
     int height = image[0].size();
     int width = image[0][0].size();
@@ -98,6 +113,9 @@ vector3d img_to_greyscale(const vector3d &image)
 
 vector3d open_image_as_greyscale()
 {
+    /*
+    opens an image in greyscale
+    */
     vector3d image, grey_image;
     image = load_image();
     grey_image = img_to_greyscale(image);
