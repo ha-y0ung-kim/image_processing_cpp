f370d3b9278aa97be5e08692b9e2c16152234664
ha-y0ung-kim <hayoungkim610@gmail.com>
2022-01-06 15:47:37 +0100

    add user select

--- main.cpp
+++ main.cpp
@@ -5,15 +5,20 @@
 #include <algorithm>
 #include <cmath>
 #include <filesystem>
+#include <memory>
 
-#include "header_files/imageload.h"
-#include "header_files/convolution.h"
-#include "header_files/filters.h"
-#include "header_files/printvector.h"
-#include "header_files/vectoroperation.h"
+// #include "header_files/imageload.h"
+// #include "header_files/convolution.h"
+// #include "header_files/filters.h"
+// #include "header_files/printvector.h"
+// #include "header_files/vectoroperation.h"
 #include "header_files/vectortypes.h"
 #include "header_files/kernel_input.h"
-#include "header_files/3d_convolution.h"
+// #include "header_files/3d_convolution.h"
+
+#include "header_files/Image.h"
+#include "header_files/Filter.h"
+#include "header_files/filterset.h"
 
 int main()
 {
@@ -29,67 +34,101 @@ int main()
         filepath = filename;
     } // Fix it
 
-    vector3d image = load_image(filename);
+    Image image(filename);
 
-    std::cout << "select filter type : " << std::endl;
-    std::cout << "Type (1) for Mean blurring" << std::endl;
-    std::cout << "Type (2) for Gaussian blurring" << std::endl;
-    std::cout << "Type (3) for Laplacian edge detection" << std::endl;
-    std::cout << "Type (4) for unsharp image" << std::endl;
+    std::cout << " select image modification method : " << std::endl;
+    std::cout << "Type (1) to blurr the image" << std::endl;
+    std::cout << "Type (2) to detect edges" << std::endl;
 
     int num;
     std::cin >> num;
 
+    std::unique_ptr<Filter> filter;
+
     if (num == 1)
     {
+
+        std::cout << "select filter type : " << std::endl;
+        std::cout << "Type (1) for Mean blurring" << std::endl;
+        std::cout << "Type (2) for Gaussian blurring" << std::endl;
+
+        int num1;
+        std::cin >> num1;
+
         int kernel_size = type_kernel_size();
-        vector2dd mean_kern;
-        mean_kern = mean_filter(kernel_size);
-        vector3d averaged_image;
-        averaged_image = convolution3d(image, mean_kern, false);
-        export_image_in_jpeg(averaged_image);
+
+        if (num1 == 1)
+        {
+            filter = std::make_unique<MeanBlur>(kernel_size);
+        }
+        else if (num1 == 2)
+        {
+            std::cout << "Type gaussian variance : " << std::endl;
+            double kernel_sigma;
+            std::cin >> kernel_sigma;
+
+            filter = std::make_unique<GaussianBlur>(kernel_size, kernel_sigma);
+        }
+        else
+        {
+            std::cout << "type a valid number " << std::endl;
+        }
     }
     else if (num == 2)
     {
-        int kernel_size = type_kernel_size();
+        image.img_to_greyscale();
+        std::cout << "select filter type : " << std::endl;
+        std::cout << "Type (1) for Laplacian filter" << std::endl;
+        std::cout << "Type (2) for sobal filter in x direction" << std::endl;
+        std::cout << "Type (3) for sobal filter in y direction" << std::endl;
 
-        std::cout << "Type gaussian variance : " << std::endl;
-        double kernel_sigma;
-        std::cin >> kernel_sigma;
-        vector2dd gaussian_kern;
-        gaussian_kern = gaussian_filter(kernel_size, kernel_sigma);
-        vector3d gaussian_image;
-        gaussian_image = convolution3d(image, gaussian_kern, false);
-        export_image(gaussian_image);
+        int num1;
+        std::cin >> num1;
+
+        if (num1 == 1)
+        {
+            int kernel_size = type_kernel_size();
+            filter = std::make_unique<Laplacian>(kernel_size);
+        }
+
+        else if (num1 == 2)
+        {
+            filter = std::make_unique<Sobal_x>(3);
+        }
+        else if (num1 == 3)
+        {
+            filter = std::make_unique<Sobal_y>(3);
+        }
+        else
+        {
+            std::cout << "type a valid number " << std::endl;
+        }
     }
-
-    else if (num == 3)
+    else
     {
-        int kernel_size = type_kernel_size();
-
-        vector2dd laplacian_kern;
-        laplacian_kern = laplacian_filter(kernel_size);
-        vector3d laplacian_image;
-        laplacian_image = convolution3d(image, laplacian_kern, true);
-        export_image(laplacian_image);
+        std::cout << " type a valid number " << std::endl;
     }
+    filter->setfilter();
+    image.convolution(*filter, true);
 
-    else if (num == 4)
+    std::cout << " select an output image extension " << std::endl;
+    std::cout << "Type (1) for PNG" << std::endl;
+    std::cout << "Type (2) for JPEG" << std::endl;
+
+    int num2;
+    std::cin >> num2;
+
+    if (num2 == 1)
     {
-        int kernel_size = type_kernel_size();
-
-        vector2dd laplacian_kern;
-        laplacian_kern = laplacian_filter(kernel_size);
-        vector3d laplacian_image, unsharp_image;
-        laplacian_image = convolution3d(image, laplacian_kern, true);
-        unsharp_image = image + 0.3 * laplacian_image;
-        export_image(unsharp_image);
+        image.export_image(FileType::PNG);
     }
 
-    // std::vector<std::vector<std::vector<uint8_t>>> edge_image_x, edge_image_y, edge_image;
-    // std::vector<std::vector<double>> sobal_kernel;
-    // sobal_kernel = sobal_filter_x();
-    // print_2dvector(sobal_kernel);
-    // edge_image_x = convolution(grey_image, sobal_kernel, true);
-    // edge_image_y = convolution(grey_image, sobal_filter_y(), true);
+    else if (num2 == 2)
+    {
+        image.export_image(FileType::JPEG);
+    }
+    else
+    {
+        std::cout << "type a valid number " << std::endl;
+    }
 }
