eb70ac85a1c3a0fcb266d5a351ced623146e560b
ha-y0ung-kim <hayoungkim610@gmail.com>
2022-01-12 23:25:45 +0100

    add const, &

--- src/classes/Filter.cpp
+++ src/classes/Filter.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 #include "Filter.h"
 
-vector2dd Filter::get_kernel()
+vector2dd Filter::get_kernel() const
 {
     return kernel;
 }
 
-bool Filter::get_negval()
+bool Filter::get_negval() const
 {
     return neg_value;
 }
